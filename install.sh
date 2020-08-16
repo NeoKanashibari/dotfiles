e82cc6a5551de0263614600187794595bcfd53b3
sebastien AUMONT <aumont.seastien@gmail.com>
2020-08-17 01:26:37 +0200

    Remove redondant link

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ for file in $(find src -mindepth 1) ; do
     [[ -d $file && ! -d $DEST/.${file#*/} ]] \
         && echo "mkdir -p $DEST/.${file#*/}" \
         && mkdir -p $DEST/.${file#*/}
-    [[ -f $file && ! -f $DEST/.${file#*/} ]] \
-        && echo "ln -s $PWD/$file $DEST/.${file#*/}" \
-        && ln -s $PWD/$file $DEST/.${file#*/}
     [[ -f $file && $(readlink -f $DEST/.${file#*/}) != $PWD/$file ]] \
         && echo "ln -sf $PWD/$file $DEST/.${file#*/}" \
         && ln -sf $PWD/$file $DEST/.${file#*/}
