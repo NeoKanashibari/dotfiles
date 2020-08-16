d88a9aaa53bf2233f2c281a64be6b5effaa9f3c7
sebastien AUMONT <aumont.seastien@gmail.com>
2020-08-16 23:14:05 +0200

    Update install script
        * Fix link creation over already existing link
        * Cleanup script

--- install.sh
+++ install.sh
@@ -1,27 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-print_info()   { printf "\e[1m" ; printf " $@" ; printf "\e[0m\n"  ; }
-print_verb()   { printf "\e[32m" ; printf " $@" ; printf "\e[0m\n" ; }
-print_warn()   { printf "\e[33m" ; printf " $@" ; printf "\e[0m\n" ; }
-print_err()    { printf "\e[31m" ; printf " $@" ; printf "\e[0m\n" ; }
-print_action() { printf "\e[1m" ; printf "> $@" ; printf "\e[0m\n" ; }
-
-DEST=$HOME
+DEST=${1:-$HOME}
 [ ! -d $DEST ] && mkdir $DEST
 
-for element in $(find src -mindepth 1) ; do
-    print_verb "for $element"
-    name=${element#*/}
-    if [[ ! -d $DEST/.$name && -d $element ]];then
-        print_action "mkdir -p $DEST/.$name"
-        mkdir -p $DEST/.$name
-    elif [[ ! -f $DEST/.$name && -f $element ]];then
-        print_action "ln -s $PWD/$element $DEST/.$name"
-        ln -s $PWD/$element $DEST/.$name
-    elif [ "$(readlink -f $PWD/$element)" != "$PWD/$element" ];then
-        print_action "ln -sf $PWD/$element $DEST/.$name"
-        ln -sf $PWD/$element $DEST/.$name
-    else
-        print_action "Nothing to do"
-    fi
+for file in $(find src -mindepth 1) ; do
+    [[ -d $file && ! -d $DEST/.${file#*/} ]] \
+        && echo "mkdir -p $DEST/.${file#*/}" \
+        && mkdir -p $DEST/.${file#*/}
+    [[ -f $file && ! -f $DEST/.${file#*/} ]] \
+        && echo "ln -s $PWD/$file $DEST/.${file#*/}" \
+        && ln -s $PWD/$file $DEST/.${file#*/}
+    [[ -f $file && $(readlink -f $DEST/.${file#*/}) != $PWD/$file ]] \
+        && echo "ln -sf $PWD/$file $DEST/.${file#*/}" \
+        && ln -sf $PWD/$file $DEST/.${file#*/}
 done
+echo "config files installed"
