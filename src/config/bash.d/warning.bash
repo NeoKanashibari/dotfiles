f92f95c5c46162a0e46c59a577835c35d06926df
sebastien AUMONT <aumont.seastien@gmail.com>
2020-08-17 14:23:36 +0200

    Force me to do it!

--- src/config/bash.d/warning.bash
+++ src/config/bash.d/warning.bash
@@ -1,6 +1,7 @@
 # check dotfiles
 pushd "$(dirname $(readlink -f ~/.bashrc))" >/dev/null
 if [ ! -z "$(git status --porcelain)" ];then
-    echo -e "\e[33m[WARN]\e[0m $(git rev-parse --show-toplevel) is not clean!"
+    rootrepo=$(git rev-parse --show-toplevel)
+    echo -e "\e[33m[WARN]\e[0m $rootrepo is not clean!"
+    popd >/dev/null ; cd $rootrepo
 fi
-popd >/dev/null
