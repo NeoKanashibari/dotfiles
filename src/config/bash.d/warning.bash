b4ab6916627d85f542fa177e1da6c97479cc960a
sebastien AUMONT <aumont.seastien@gmail.com>
2020-08-17 14:29:43 +0200

    (bash) make warrning stay on /home/kanashibari dir if no problem detected

--- src/config/bash.d/warning.bash
+++ src/config/bash.d/warning.bash
@@ -4,4 +4,6 @@ if [ ! -z "$(git status --porcelain)" ];then
     rootrepo=$(git rev-parse --show-toplevel)
     echo -e "\e[33m[WARN]\e[0m $rootrepo is not clean!"
     popd >/dev/null ; cd $rootrepo
+else
+    popd >/dev/null
 fi
