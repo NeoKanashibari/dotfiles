c2c7ae50f80ab0a129a90322565ea2817d74e331
Aumont Sebastien <aumont.sebastien@idemia.com>
2020-08-20 16:33:10 +0200

    (bash) Don't do warning on tty and start X only on tty1

--- src/config/bash.d/warning.bash
+++ src/config/bash.d/warning.bash
@@ -1,6 +1,6 @@
 # check dotfiles
 pushd "$(dirname $(readlink -f ~/.bashrc))" >/dev/null
-if [ ! -z "$(git status --porcelain)" ];then
+if [[ ! -z "$(git status --porcelain)" && ! -z $DISPLAY ]];then
     rootrepo=$(git rev-parse --show-toplevel)
     echo -e "\e[33m[WARN]\e[0m $rootrepo is not clean!"
     popd >/dev/null ; cd $rootrepo
