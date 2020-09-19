c5e449ae887415982e745a9727be1d287955dd29
sebastien AUMONT <aumont.seastien@gmail.com>
2020-09-19 04:29:06 +0200

    (bash) Add git aliases from strager repo

---
+++ src/config/bash.d/git_aliases.bash
@@ -0,0 +1,27 @@
+gs() {
+  git status -s -b "${@}"
+}
+gc() {
+  git commit -v "${@}"
+}
+g.() {
+git add -p "${@}"
+}
+gd() {
+  git diff "${@}"
+}
+gp() {
+  git checkout -p "${@}"
+}
+gr() {
+  git rebase "${@}"
+}
+grc() {
+  git rebase --continue "${@}"
+}
+gar() {
+  git add --all .
+}
+ga() {
+  git commit --amend --reuse-message=HEAD
+}
