c039b261fdd59f0df219f7af0d357ed33ee1877b
sebastien AUMONT <aumont.seastien@gmail.com>
2020-11-24 01:11:52 +0100

    (bspwm) Cleanup monitor detection

--- src/config/bash.d/warning.bash
+++ src/config/bash.d/warning.bash
@@ -7,3 +7,7 @@ if [[ ! -z "$(git status --porcelain)" && ! -z $DISPLAY ]];then
 else
     popd >/dev/null
 fi
+
+if [[ ! -f $HOME/.config/currentlayout.sh ]]; then
+  echo -e "\e[33m[WARN]\e[0m layout not set at $HOME/.config/currentlayout.sh!"
+fi
