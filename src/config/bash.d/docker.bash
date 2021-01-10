19840dbe1c18709ccdc41686b94fc7a2465a6ff3
Sebastien Aumont <s.aumont@groupecarrus.com>
2021-01-10 13:17:28 +0100

    (bash) Add/Cleanup `dk-*` docker-helper function
        - dk-nuke
        - dk-up
        - dk-down
        - dk-cleanup

--- src/config/bash.d/docker.bash
+++ src/config/bash.d/docker.bash
@@ -1,4 +1,16 @@
-dcleanup(){
+dk-nuke() {
+    docker system prune --force -a --volumes
+}
+
+dk-cleanup() {
     docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
     docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+}
+
+dk-down() {
+    docker-compose down
+}
+
+dk-up() {
+    docker-compose up
 }
