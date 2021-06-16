cc243d6b9cf3eeccf40ba3bfd415412549b6213f
Sebastien Aumont <s.aumont@groupecarrus.com>
2021-06-16 19:47:00 +0200

    (bash) Add ssh helper

--- src/config/bash.d/sshs.bash
+++ src/config/bash.d/sshs.bash
@@ -4,3 +4,11 @@ function sshs() {
     ssh-add $priv
   done
 }
+
+function sshwithbashrc() {
+  declare -f > /tmp/bashall
+  alias >> /tmp/bashall
+  scp /tmp/bashall $1:/tmp/.bashrc_temp
+  ssh -t $1 "bash --rcfile /tmp/.bashrc_temp ; rm /tmp/.bashrc_temp"
+}
+
