0b69e13b1da764efe4b5f907e854a391735a085d
Sebastien AUMONT <s.aumont@groupecarrus.com>
2021-10-19 03:09:39 +0200

    (bash) Update warning message

--- src/config/bash.d/warning.bash
+++ src/config/bash.d/warning.bash
@@ -1,12 +1,10 @@
 # check dotfiles
 pushd "$(dirname $(readlink -f ~/.bashrc))" >/dev/null
-if [[ ! -z "$(git status --porcelain)" && ! -z $DISPLAY ]];then
+if [[ ! -z "$(git status --porcelain)" ]];then
     rootrepo=$(git rev-parse --show-toplevel)
-    echo -e "\e[33m[WARN]\e[0m $rootrepo is not clean!"
-    popd >/dev/null ; cd $rootrepo
-else
-    popd >/dev/null
+    echo -e "\e[33m[WARN]\e[0m commit your config files!"
 fi
+popd >/dev/null
 
 if [[ ! -f $HOME/.config/currentlayout.sh ]]; then
   echo -e "\e[33m[WARN]\e[0m layout not set at $HOME/.config/currentlayout.sh!"
