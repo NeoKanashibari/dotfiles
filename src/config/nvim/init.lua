c312907e7476ed80cf87508d429607452d63db68
Sebastien AUMONT <s.aumont@groupecarrus.com>
2022-11-23 23:32:35 +0100

    (nvim) Check if packer is not install

--- src/config/nvim/init.lua
+++ src/config/nvim/init.lua
@@ -1,9 +1,16 @@
-require('packer-config');
+function packer_present()
+    local f=io.open(os.getenv("HOME") .. "/.local/share/nvim/site/pack/packer/start/packer.nvim","r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
 require('config/vim');
-
-require('config/color');
-require('config/status-line');
-
-
-require('config/lsp');
-require('config/nvim-cmp');
+if packer_present() then 
+    require('packer-config');
+    require('config/status-line');
+    require('config/lsp');
+    require('config/nvim-cmp');
+    require('config/color');
+else
+    print("packer not installed, use git command to install packer")
+    print("git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim")
+end
