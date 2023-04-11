b0b04caa9e4d1f1295f8476d50ade69422a005da
Sebastien AUMONT <aumont.sebastien@gmail.com>
2023-04-11 12:32:07 +0200

    (prompt) Simplify prompt

--- src/config/bash.d/prompt.bash
+++ src/config/bash.d/prompt.bash
@@ -1,15 +1,13 @@
 export PS1='[\u@\h \w]\$'
 export PS2='  '
 export PS3='Select : '
-export PS4='\033[1;35m ${BASH_SOURCE}:\033[1;31m${LINENO}\033[0m:\033[1;36m${FUNCNAME[0]:+${FUNCNAME[0]}()}\033[0m: '
+#export PS4='\033[1;35m ${BASH_SOURCE}:\033[1;31m${LINENO}\033[0m:\033[1;36m${FUNCNAME[0]:+${FUNCNAME[0]}()}\033[0m: '
+export PS4='|-'
 
-# Change default prompt
-if [[ -f "$SOURCECONFIG/software/pureline/pureline" && $(tty) == *'pts'* ]];then
-    source $SOURCECONFIG/software/pureline/pureline ~/.config/pureline.conf
-elif [ -f /usr/lib/git-core/git-sh-prompt ];then
-    source /usr/lib/git-core/git-sh-prompt
-    PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
-elif [ -f /usr/share/git/git-prompt.sh ];then
-    source /usr/share/git/git-prompt.sh
-    PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
-fi
+function parse_git_dirty {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working tree clean" ]] && echo "*"
+}
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
+}
+export PS1='\[\e[\033[01;36m\]\u@\h \[\e[36;5;34m\]\W\[\e[\033[38;5;47m\] $(parse_git_branch)\[\e[\033[00m\]\$ '
--- src/config/bash.d/warning.bash
+++ src/config/bash.d/warning.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 # check dotfiles
 cd "$HOME" || exit
-pushd $(dirname $(readlink -f ~/.bashrc)) > /dev/null
-if [[ -n "$(git status --porcelain)" ]];then
-    echo -e "\e[33m[WARN]\e[0m commit your config files!"
-fi
-popd >/dev/null
 
 if [[ ! -f $HOME/.config/currentlayout.sh ]]; then
   echo -e "\e[33m[WARN]\e[0m layout not set at $HOME/.config/currentlayout.sh !"
