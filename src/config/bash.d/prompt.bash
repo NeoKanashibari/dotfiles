4776fab956ccb283145b15bbd05fccbf022a93ae
Sebastien AUMONT <aumont.sebastien@gmail.com>
2024-07-28 01:01:56 +0200

    bash: update prompt

--- src/config/bash.d/prompt.bash
+++ src/config/bash.d/prompt.bash
@@ -4,6 +4,8 @@ export PS1='[\u@\h \w]\$'
 export PS2='  '
 export PS3='Select : '
 
-#TODO: use termstatus if available otherwide git-prompt otherwide simple PS1
+if command -v termstatus &> /dev/null;then
+    PS1='$(termstatus $?)'
+fi
+#TODO: otherwide git-prompt 
 #source /usr/share/git/git-prompt.sh
-PS1='$(termstatus $?)'
